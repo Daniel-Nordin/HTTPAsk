6139550e8b440dc6e224da1e86d79b7dbec09de9
Daniel Nordin <Daniel.Nordin@users.noreply.github.com>
2020-03-02 17:16:21 +0100

    Fixed bad request message

--- java/src/HTTPAsk.java
+++ java/src/HTTPAsk.java
@@ -23,12 +23,12 @@ public class HTTPAsk {
                 StringBuilder sb = new StringBuilder();
 
                 try {
-                    if (params[0].equals("GET")) {
+                    if (params[0].equals("GET") && (params[7].equals("HTTP")|| params[9].equals("HTTP"))) {
                         if (params[2].equals("ask") && params[3].equals("hostname") && params[5].equals("port")) {
                             if (params.length == 9) {
                                 sb.append("HTTP/1.1 200 OK\r\n\r\n");
                                 sb.append(TCPClient.askServer(params[4], Integer.parseInt(params[6])));
-                            } else if (params.length == 11 && params[7] == "string") {
+                            } else if (params.length == 11 && params[7].equals("string")) {
                                 sb.append("HTTP/1.1 200 OK\r\n\r\n");
                                 sb.append(TCPClient.askServer(params[4], Integer.parseInt(params[6]), params[8]));
                             }
